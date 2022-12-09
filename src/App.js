bc463d5724dc6785d9ad4cb0e3b9c80d5a0a38c5
John Million <john.million@rtslabs.com>
2022-12-09 16:22:39 -0500

    Experimenting with reusable components and props

--- src/App.js
+++ src/App.js
@@ -1,4 +1,34 @@
+import 'bulma/css/bulma.css';
+import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
+
 export default function App() {
 
-    return <input placeholder='bye there'/>;
+    return (
+      <div>
+        <div>Personal Digital Assistants</div>
+  
+        <div className="container">
+          <section className="section">
+            <div className="columns">
+              <div className="column is-4">
+                <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+              </div>
+              <div className="column is-4">
+                <ProfileCard
+                  title="Cortana"
+                  handle="@cortana32"
+                  image={CortanaImage}
+                />
+              </div>
+              <div className="column is-4">
+                <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+    );
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+
 import ReactDom from 'react-dom/client';
 import App from './App';
 
